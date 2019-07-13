97bd59030952f1d7fdbb74f5834c9251fad04f81
Jamael17 <juancurvelo99@gmail.com>
2019-07-13 18:18:12 -0430

    Updates

---
+++ precache-manifest.bbeb0edd5dd4df94f7737528685cbfd3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd28d0a5f1855f59a5c88efb80552cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c1481f0419f558a61b3",
+    "url": "/static/css/main.52e8b164.chunk.css"
+  },
+  {
+    "revision": "6cbf32e6e7458f998581",
+    "url": "/static/js/2.5646c6a5.chunk.js"
+  },
+  {
+    "revision": "8c1481f0419f558a61b3",
+    "url": "/static/js/main.6f39b8e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7b3f37886a6ce7648e04981e1d3c38f6",
+    "url": "/static/media/Optimized-aleks-dorohovich-background.7b3f3788.jpg"
+  },
+  {
+    "revision": "c14197607771ffa7f65c893b0407d2fc",
+    "url": "/static/media/Optimized-emile-perron-pcBackground.c1419760.jpg"
+  },
+  {
+    "revision": "fd103945c6aec843e9b4a04534f6112a",
+    "url": "/static/media/expressJsLogo.fd103945.jpg"
+  },
+  {
+    "revision": "3f25b7cc49bb4e9577fe372fde253130",
+    "url": "/static/media/fatimasImg.3f25b7cc.png"
+  },
+  {
+    "revision": "454a400ca987d4271e54045982ea3876",
+    "url": "/static/media/htmlLogo.454a400c.png"
+  },
+  {
+    "revision": "5b47d4f8d197911857e8e77cb4dd1569",
+    "url": "/static/media/nodejsLogo.5b47d4f8.png"
+  },
+  {
+    "revision": "902b83ed1a42964f9a8255b246b1bf12",
+    "url": "/static/media/p5jsLogo.902b83ed.jpg"
+  },
+  {
+    "revision": "c5ffdc8a0814fabd9c978239f55c0ef9",
+    "url": "/static/media/photoshopLogo.c5ffdc8a.png"
+  },
+  {
+    "revision": "d6254a52e58310ce020354534b0c0998",
+    "url": "/static/media/phpLogo.d6254a52.png"
+  },
+  {
+    "revision": "3a040081c26eaf8e6e807db669bc917e",
+    "url": "/static/media/portfolioImg.3a040081.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/reactLogo.47ce6e77.png"
+  },
+  {
+    "revision": "288caf89d9e32474055cc737fe86f763",
+    "url": "/static/media/socketIoLogo.288caf89.png"
+  },
+  {
+    "revision": "7c4ff28b8546efde2912d325dd617bd5",
+    "url": "/static/media/stickserImg.7c4ff28b.png"
+  },
+  {
+    "revision": "59f5f1216013fb89cfe6406a5482a35e",
+    "url": "/static/media/tiseguridadImg.59f5f121.png"
+  },
+  {
+    "revision": "598d8e6da50ecfb4b316cd7503d79314",
+    "url": "/static/media/webDesing-bg.598d8e6d.jpg"
+  }
+]);
